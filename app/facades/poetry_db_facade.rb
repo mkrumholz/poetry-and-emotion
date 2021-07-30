fcdbe3df5edca5a1f4e82b1cd9c85b92a0a39880
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-30 11:08:30 -0600

    Create PoetryDbFacade method #top_10_poems_by

--- app/facades/poetry_db_facade.rb
+++ app/facades/poetry_db_facade.rb
@@ -2,9 +2,6 @@ class PoetryDbFacade
   def self.top_10_poems_by(author)
     response = PoetryDbService.poems_by(author)
     all_poems = response.map do |poem_details|
-      content = poem_details[:lines.join].join(' / ')
-      details = {}
-      require 'pry'; binding.pry
       Poem.new(poem_details)
     end
     all_poems.first(10)
