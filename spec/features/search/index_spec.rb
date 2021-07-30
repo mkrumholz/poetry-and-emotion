e39cd8d0150fd5bd9fd46660b41e9f72698f1628
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-30 10:22:13 -0600

    Create route to search index from welcome screen

---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe 'Search result index' do
+
+  it 'displays a list of 10 poems'
+end
--- spec/features/user_can_visit_root_path_spec.rb
+++ spec/features/user_can_visit_root_path_spec.rb
@@ -8,4 +8,13 @@ describe "User can visit root path" do
     expect(page).to have_content('Welcome to Poetry and Emotion')
     expect(page).to have_selector('input')
   end
+
+  it 'links to the search results index' do
+    visit '/'
+
+    fill_in :author, with: 'Emily'
+    click_on 'Get Poems'
+
+    expect(current_path).to eq search_path
+  end
 end
