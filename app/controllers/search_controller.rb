9a8e4b2d21539c7ca238614910971472d34b0c22
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-30 10:32:27 -0600

    Create scaffolding for poetry service

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def index
-    
+    @poems = PoetryDbFacade.top_10_poems_by(params[:author])
   end
 end
--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Search result index' do
+  it 'displays a list of 10 poems' do
+    visit '/'
 
-  it 'displays a list of 10 poems'
+    fill_in :author, with: 'Emily'
+    click_on 'Get Poems'
+
+    expect(current_path).to eq search_path
+    expect(page).to have_content "Not at Home to Callers"
+    expect(page).to have_content "Emily Dickinson"
+    expect(page).to have_content "Not at Home to Callers / Says the Naked Tree -- / Bonnet due in April -- / Wishing you Good Day --"
+  end
 end
